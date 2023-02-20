c74f84f48f95de7ca927efcfee154025b4958b0a
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2023-02-20 16:26:22 +0600

    task one solved

--- task-one.php
+++ task-one.php
@@ -1,3 +1,16 @@
 <!-- Write a PHP function to sort an array of strings by their length, in ascending order. (Hint: You can use the usort() function to define a custom sorting function.) -->
 
 <?php
+
+function sortString($arr)
+{
+    usort($arr, function ($a, $b) {
+        return strlen($a) - strlen($b);
+    });
+
+    return $arr;
+}
+
+$array_of_string = ["apple", "banana", "jackfruit", "oreo", "mango"];
+
+print_r(sortString($array_of_string));
