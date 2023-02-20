6237a357e3d60bd4b5e5172218bf101e8f4f5b96
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2023-02-20 17:48:11 +0600

    task four solved

--- task-four.php
+++ task-four.php
@@ -1,1 +1,23 @@
 <!-- Write a PHP function to check if a string contains only letters and whitespace. -->
+
+<?php
+
+function checkString($str)
+{
+    return preg_match('/^[A-Za-z\s]+$/', $str);
+}
+
+$str1 = 'This is a string containing only letter and whitespace';
+$str2 = 'This is string 2 containing only letter, whitespace, number and exclamation!!';
+
+// if (checkString($str1)) {
+//     echo "nothing except letter and whitespace!";
+// } else {
+//     echo "contains more character!";
+// }
+
+if (checkString($str2)) {
+    echo "nothing except letter and whitespace!";
+} else {
+    echo "contains more character!";
+}
